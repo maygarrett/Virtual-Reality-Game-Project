9ff1a1dc17a53726121ceb9a983b3f9ff8ff882f
YEC\Garrett.May <maygarrett@hotmail.com>
2018-01-23 09:57:11 -0500

    [Mountain Scene] - Added a first person camera with movement to oberve the scene without VR connected

---
+++ Virtual Reality Game Project/Assets/Scripts/SubstitutePlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubstitutePlayerMovement : MonoBehaviour {
+
+    private Transform _subPlayer;
+
+    private float _movementSpeed = 3.0f;
+    private float _rotationSpeed = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+        _subPlayer = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKey(KeyCode.UpArrow))
+        {
+            _subPlayer.Translate(Vector3.forward * _movementSpeed * 1/30);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            _subPlayer.Rotate(-transform.up * _rotationSpeed * 1/30);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _subPlayer.Rotate(transform.up * _rotationSpeed * 1/30);
+        }
+    }
+}
