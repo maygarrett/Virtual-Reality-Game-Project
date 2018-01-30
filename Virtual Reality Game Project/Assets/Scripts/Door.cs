dddf8ca706ae04678da39d89dfd376ee88279239
YEC\Garrett.May <maygarrett@hotmail.com>
2018-01-30 11:04:31 -0500

    [Key] - added key object to scene with prefab [Door] - added door object to scene with OpenDoor() function that works [Padlock] - added padlock object to scene attached to door object that calls openDoor when colliding with key object, deletes key and lock after use

---
+++ Virtual Reality Game Project/Assets/Scripts/Door.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    // door rotation variables
+    private Vector3 _rotationAxis = new Vector3(0, 1, 0);
+    private float _rotationAngle = -4.0f;
+    [SerializeField] private Transform _rotationPoint;
+
+    private bool _isOpen;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // just for testing
+        /*if (!_isOpen)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                OpenDoor();
+            }
+        }*/
+    }
+
+    public void OpenDoor()
+    {
+        int tempCounter = 0;
+        while (tempCounter < (30 * 100))
+        {
+            transform.RotateAround(_rotationPoint.position, _rotationAxis, _rotationAngle / 100);
+            tempCounter++;
+        }
+        _isOpen = true;
+    }
+}
---
+++ Virtual Reality Game Project/Assets/Scripts/Lock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour {
+
+    [SerializeField] private GameObject _door;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision c)
+    {
+        if(c.gameObject.tag == "Key")
+        {
+            _door.gameObject.GetComponent<Door>().OpenDoor();
+            Destroy(c.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
